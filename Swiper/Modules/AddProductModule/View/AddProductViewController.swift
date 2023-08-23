2f1feac852e41ab728a50b6d48aa0aecc8ba20d6
Aditya Khavanekar <adityakhavanekar98@gmail.com>
2023-08-23 12:09:03 +0530

    Code Refactoring

--- Swiper/Modules/AddProductModule/View/AddProductViewController.swift
+++ Swiper/Modules/AddProductModule/View/AddProductViewController.swift
@@ -14,8 +14,10 @@ class AddProductViewController: UIViewController {
     @IBOutlet weak var productTypeTxtField: UITextField!
     @IBOutlet weak var productNameTxtField: UITextField!
     
+    @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var internalView: UIView!
+    
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
     
@@ -23,21 +25,29 @@ class AddProductViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        internalView.layer.cornerRadius = 10
-        internalView.applyLiftedShadowEffectToView(cornerRadius: 5)
-        cancelButton.layer.cornerRadius = 5
+        setupUI()
+    }
+    
+    private func setupUI(){
+        internalView.clipsToBounds = true
         cancelButton.clipsToBounds = true
-        cancelButton.applyLiftedShadowEffectToView(cornerRadius: 5)
-        addButton.layer.cornerRadius = 5
         addButton.clipsToBounds = true
+        
+        addButton.layer.cornerRadius = 5
+        cancelButton.layer.cornerRadius = 5
+        internalView.layer.cornerRadius = 10
+        
+        internalView.applyLiftedShadowEffectToView(cornerRadius: 5)
+        cancelButton.applyLiftedShadowEffectToView(cornerRadius: 5)
         addButton.applyLiftedShadowEffectToView(cornerRadius: 10)
+        
+        setTextFieldDelegates(textFields: [productNameTxtField,productTypeTxtField,priceTxtField,taxTxtField])
+        addScrollingObeservers()
+    }
+    
+    private func addScrollingObeservers(){
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardDisappear), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        productNameTxtField.delegate = self
-        productTypeTxtField.delegate = self
-        priceTxtField.delegate = self
-        taxTxtField.delegate = self
     }
     
     @objc func keyboardAppear(){
@@ -52,6 +62,12 @@ class AddProductViewController: UIViewController {
         print(scrollView.contentSize.height)
         self.scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.internalView.frame.height + 50)
         isExpand = false
+    }
+    
+    private func setTextFieldDelegates(textFields:[UITextField]){
+        for txtField in textFields{
+            txtField.delegate = self
+        }
     }
     
     @IBAction func cancelButtonClicked(_ sender: UIButton) {
